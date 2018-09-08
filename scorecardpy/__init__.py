9194960c6d8a49dffb3f230d0af29548696be6cb
shichenxie <shichen_xie@qq.com>
2018-09-09 00:17:22 +0800

    split_df

--- scorecardpy/__init__.py
+++ scorecardpy/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding:utf-8 -*- 
 
-from .germancredit import germancredit
-from .splitdf import split_df
-from .info_value import iv
+from scorecardpy.germancredit import germancredit
+from scorecardpy.split_df import split_df
+from scorecardpy.info_value import iv
 # from .info_ent_indx_gini import (ig, ie)
-from .var_filter import var_filter
-from .woebin import (woebin, woebin_ply, woebin_plot, woebin_adj)
-from .perf import (perf_eva, perf_psi)
-from .scorecard import (scorecard, scorecard_ply)
+from scorecardpy.var_filter import var_filter
+from scorecardpy.woebin import (woebin, woebin_ply, woebin_plot, woebin_adj)
+from scorecardpy.perf import (perf_eva, perf_psi)
+from scorecardpy.scorecard import (scorecard, scorecard_ply)
 
