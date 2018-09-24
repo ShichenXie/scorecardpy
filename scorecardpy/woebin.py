2f8bb7f50a7b6c80667d3dd582f0e8a3986fd022
shichenxie <shichen_xie@qq.com>
2018-09-24 12:38:47 +0800

    woebin positive values

--- scorecardpy/woebin.py
+++ scorecardpy/woebin.py
@@ -723,6 +723,9 @@ def woebin(dt, y, x=None, breaks_list=None, special_values=None,
     `woebin` generates optimal binning for numerical, factor and categorical variables 
     using methods including tree-like segmentation or chi-square merge. 
     woebin can also customizing breakpoints if the breaks_list or special_values was provided.
+    The default woe is defined as ln(Distr_Bad_i/Distr_Good_i). If you 
+    prefer ln(Distr_Good_i/Distr_Bad_i), please set the argument `positive` 
+    as negative value, such as '0' or 'good'.
     
     Params
     ------
