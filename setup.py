1712e266ebcf0851c90da64f03865b0fa561d43a
Shichen Xie <shichen_xie@qq.com>
2019-11-09 20:35:36 +0800

    0.1.9.1

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='credit scorecard, woe binning, performace evaluation',  # Optional
     packages=['scorecardpy'],  # Required
